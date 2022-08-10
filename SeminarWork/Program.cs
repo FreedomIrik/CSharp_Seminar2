168a554dd56f3f87ab7410c0db6811584ca5161b
Irik <freedomirik@yandex.ru>
2022-08-10 21:08:42 +0500

    Задача №12

--- SeminarWork/Program.cs
+++ SeminarWork/Program.cs
@@ -31,3 +31,24 @@ int result = firstDigit*10 + secondDigit;
 
 System.Console.WriteLine(result);*/
 
+/* Напишите программу, которая будет принимать на вход два числа и выводить,
+является ли второе число кратным первому. Если число 2 не кратно числу 1, 
+то программа выводит остаток от деления. 
+34, 5 -> не кратно, остаток 4 
+16, 4 -> кратно */
+
+System.Console.WriteLine("Введите первое число: ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число: ");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstnumber % secondnumber == 0 )
+{
+    System.Console.WriteLine("Кратно");
+}
+else
+{
+    int remain = firstnumber % secondnumber;
+    System.Console.WriteLine($"Не кратно, остаток {remain}");
+}
